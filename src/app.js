757ca1b002f8e5a277f7842ba0918f70d5973271
elusiveellie <elena@realemail.net>
2024-01-08 20:59:03 -0500

    Added changes for deployment

--- src/app.js
+++ src/app.js
@@ -1,7 +1,14 @@
 const express = require("express");
 const app = express();
+const pastesRouter = require("./pastes/pastes.router");
+const usersRouter = require("./users/users.router");
 
 // TODO: Follow instructions in the checkpoint to implement ths API.
+
+app.use(express.json());
+
+app.use("/pastes", pastesRouter); // Note: app.use
+app.use("/users", usersRouter);
 
 // Not found handler
 app.use((request, response, next) => {
@@ -11,7 +18,8 @@ app.use((request, response, next) => {
 // Error handler
 app.use((error, request, response, next) => {
   console.error(error);
-  response.send(error);
+  const { status = 500, message = "Something went wrong!" } = error;
+  response.status(status).json({ error: message });
 });
 
 module.exports = app;
